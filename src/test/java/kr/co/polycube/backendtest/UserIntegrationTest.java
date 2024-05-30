00a236d94df1224505a11753670e010b217627e2
Kim Taejin <rlaxowls0504@gmail.com>
2024-05-31 03:50:46 +0900

    feat: create user inquiry integration test

--- src/test/java/kr/co/polycube/backendtest/UserIntegrationTest.java
+++ src/test/java/kr/co/polycube/backendtest/UserIntegrationTest.java
@@ -2,8 +2,10 @@ package kr.co.polycube.backendtest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.co.polycube.backendtest.dto.UserRequestDto;
+import kr.co.polycube.backendtest.entity.User;
 import kr.co.polycube.backendtest.repository.UserRepository;
 import kr.co.polycube.backendtest.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -31,17 +34,34 @@ public class UserIntegrationTest {
     @Autowired
     private UserRepository userRepository;
 
+    private User user;
+
+    @BeforeEach()
+    void setUp() {
+        user = userRepository.save(new User("John Doe"));
+    }
+
     @Test
     @DisplayName("user 등록 통합 테스트")
     void testCreateUser() throws Exception {
         UserRequestDto requestDto = new UserRequestDto();
-        requestDto.setName("John Doe");
+        requestDto.setName("test user1");
 
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestDto)))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("user 조회 통합 테스트")
+    void testGetUser() throws Exception {
+        mockMvc.perform(get("/users/" + user.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(user.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(user.getName()))
                 .andDo(print());
     }
 }
