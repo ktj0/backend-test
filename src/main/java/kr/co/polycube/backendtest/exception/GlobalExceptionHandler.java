e365d26e2a49c03e388947a8998b4926cd515326
Kim Taejin <rlaxowls0504@gmail.com>
2024-05-31 04:07:50 +0900

    feat: create GlobalExceptionHandler

---
+++ src/main/java/kr/co/polycube/backendtest/exception/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package kr.co.polycube.backendtest.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    // 잘못된 요청 예외 처리
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleBadRequest(Exception ex) {
+        Map<String, String> response = new HashMap<>();
+
+        response.put("reason", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    // 존재하지 않는 API 호출 예외 처리
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Map<String, String>> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+
+        response.put("reason", "Resource not found");
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    // validation 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, List<String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<String> errorMessage = new ArrayList<>();
+        Map<String, List<String>> response = new HashMap<>();
+
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        for (FieldError fieldError : fieldErrors) {
+            errorMessage.add(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+        }
+
+        response.put("reason", errorMessage);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    // entity 가 존재하지 않을 때 예외 처리
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Map<String, String>> handleEntityNotFoundException(EntityNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+
+        response.put("reason", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
